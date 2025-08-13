a424e1b5fa3771427b85c7f977399a1ebfb9bc36
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2025-08-13 12:47:49 +0800

    finished partial events

--- src/app/(routes)/sk-official/events-activities/_components/client.tsx
+++ src/app/(routes)/sk-official/events-activities/_components/client.tsx
@@ -1,13 +1,13 @@
 import { Events } from "@prisma/client";
+import Image from 'next/image';
 import React from "react";
-import Empty from "@/components/globals/empty";
 
 const Client = ({ data }: { data: Events[] }) => {
   return (
     <div>
       {data.length === 0 ? (
         <div className="flex h-screen items-center flex-col justify-center">
-          <Empty />
+          <Image src="/empty.svg" alt='Empty' width={150} height={150} />
           <h3 className="text-lg font-medium mb-2">No events found.</h3>
           <p className="text-muted-foreground">
             Please check back later for updates.
